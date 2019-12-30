164bc42847775363226087d0494e7082b54afcb7
Sahib Dhanjal <dhanjalsahib@gmail.com>
2019-12-30 14:05:32 -0800

    working and responsive navbar - hamburger bug

--- js/main.js
+++ js/main.js
@@ -2,6 +2,29 @@
  * Transcend - Main JS
  *
  * ------------------------------------------------------------------- */
+(function($) { // Begin jQuery
+    $(function() { // DOM ready
+        $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // Close one dropdown when selecting another
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+        });
+        // Clicking away from dropdown will remove the dropdown class
+        $('html').click(function() {
+        $('.nav-dropdown').hide();
+        });
+        // Toggle open and close nav styles on click
+        $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+        });
+        // Hamburger to X toggle
+        $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+        });
+    });
+})(jQuery);
+
 
 (function($) {
 
@@ -73,7 +96,6 @@
     };
 
 
-
    /* Masonry
     * ---------------------------------------------------- */
     var clMasonryFolio = function () {
@@ -106,7 +128,6 @@
             disable: 'mobile'
         });
     };
-
 
 
    /* Initialize
