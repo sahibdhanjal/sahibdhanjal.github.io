348ddbef6867510cfd5bb126bd8c6150e4708787
Sahib Dhanjal <dhanjalsahib@gmail.com>
2019-12-30 14:20:39 -0800

    nav toggle working

--- js/main.js
+++ js/main.js
@@ -5,22 +5,22 @@
 (function($) { // Begin jQuery
     $(function() { // DOM ready
         $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        // Close one dropdown when selecting another
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
+            $(this).siblings('.nav-dropdown').toggle();
+            // Close one dropdown when selecting another
+            $('.nav-dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
         });
         // Clicking away from dropdown will remove the dropdown class
         $('html').click(function() {
-        $('.nav-dropdown').hide();
+            $('.nav-dropdown').hide();
         });
         // Toggle open and close nav styles on click
         $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
+            $('nav ul').slideToggle();
         });
         // Hamburger to X toggle
         $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
+            this.classList.toggle('active');
         });
     });
 })(jQuery);
